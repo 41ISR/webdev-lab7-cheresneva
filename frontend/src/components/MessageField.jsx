1cd211f503c474c11d6dc890c02109912d6891ab
Хуйцын <leveychel@gmail.com>
2025-11-26 07:12:06 +0000

    finish

--- frontend/src/components/MessageField.jsx
+++ frontend/src/components/MessageField.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { api } from "../api/api"
 import { useMessageStore } from "../store/useMessageStore"
 import Button from "./Button"
@@ -7,11 +8,13 @@ const MessageField = () => {
     const { getMessages } = useMessageStore()
     const handleSubmit = async (e) => {
         e.preventDefault()
+        
         const message = { content: e.target.content.value }
 
         try {
             await api.sendMessage(message)
             await getMessages()
+            e.target.reset()
         } catch (error) {
             console.error(error)
         }
